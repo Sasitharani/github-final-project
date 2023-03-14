bb867742ef899bf0468ef92dd3cd897d36de4a71
Sasitharani <50744050+Sasitharani@users.noreply.github.com>
2023-03-14 21:41:44 +0530

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -5,6 +5,7 @@
 # Do not use this in production. Sample purpose only.
 
 # Author: Upkar Lidder (IBM)
+#################################################### Author:Sasitharani################################################################
 # Additional Authors:
 # A name
 
